bf7fc17cf24ec76fa09ab5e8b675c22feadaeca6
cihanss <25405420+cihanss@users.noreply.github.com>
2020-07-31 14:03:30 +0200

    changes for enabling update and deactivation (#3)
    * update input options
    - add state check option with job-id
    - add input field for identifier
    
    * update dependencies
    
    * unify the style of components
    
    * remove identifier field from the register data

--- src/components/RegistrarInput.jsx
+++ src/components/RegistrarInput.jsx
@@ -1,8 +1,7 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import axios from 'axios';
 
-import { Segment, Item, Label, Divider, Button, Input, TextArea, List, Grid } from 'semantic-ui-react';
-import { Highlight } from 'react-fast-highlight';
+import {Button, Divider, Form, Grid, Input, Item, Label, List, Segment, Table, TextArea} from 'semantic-ui-react';
 
 import AddPublicKey from './add/AddPublicKey';
 import AddService from './add/AddService';
@@ -12,20 +11,50 @@ export class RegistrarInput extends Component {
 
 	constructor(props) {
 		super(props)
-		this.state = { operation: null, driverId: null, driverName: null, jobId: null, options: null, secret: null, addServices: [], addPublicKeys: [], addAuthentications: [] };
+		this.state = {
+			operation: null,
+			driverId: null,
+			driverName: null,
+			jobId: null,
+			identifier: null,
+			options: null,
+			secret: null,
+			addServices: [],
+			addPublicKeys: [],
+			addAuthentications: []
+		};
 	}
 
 	execute() {
-		var data = {
-			'jobId': this.state.jobId,
-			'options': JSON.parse(this.state.options),
-			'secret': JSON.parse(this.state.secret),
-			'didDocument': {
-				'service': this.state.addServices,
-				'publicKey': this.state.addPublicKeys,
-				'authentication': this.state.addAuthentications
-			}
-		};
+		var data;
+
+		if ("update" === this.state.operation || "deactivate" === this.state.operation) {
+			data = {
+				'jobId': this.state.jobId,
+				'identifier': this.state.identifier,
+				'options': JSON.parse(this.state.options),
+				'secret': JSON.parse(this.state.secret),
+				'didDocument': {
+					'service': this.state.addServices,
+					'publicKey': this.state.addPublicKeys,
+					'authentication': this.state.addAuthentications
+				}
+			};
+		}
+
+		if ("register" === this.state.operation) {
+			data = {
+				'jobId': this.state.jobId,
+				'options': JSON.parse(this.state.options),
+				'secret': JSON.parse(this.state.secret),
+				'didDocument': {
+					'service': this.state.addServices,
+					'publicKey': this.state.addPublicKeys,
+					'authentication': this.state.addAuthentications
+				}
+			};
+		}
+
 		axios
 			.post(env.backendUrl + '1.0/' + this.state.operation + '?' + 'driverId=' + encodeURIComponent(this.state.driverId), JSON.stringify(data))
 			.then(response => {
@@ -33,7 +62,7 @@ export class RegistrarInput extends Component {
 				const jobId = response.data.jobId;
 				const registrarMetadata = response.data.registrarMetadata;
 				const methodMetadata = response.data.methodMetadata;
-				this.setState({ jobId: jobId });
+				this.setState({jobId: jobId});
 				this.props.onResult(didState, jobId, registrarMetadata, methodMetadata);
 			})
 			.catch(error => {
@@ -63,15 +92,39 @@ export class RegistrarInput extends Component {
 	}
 
 	onClickRegister(driverId, driverName) {
-		this.setState({ operation: 'register', driverId: driverId, driverName: driverName, jobId: null, options: this.defaultOptions('register', driverId), secret: this.defaultSecret('register', driverId) });
+		this.setState({
+			operation: 'register',
+			driverId: driverId,
+			driverName: driverName,
+			jobId: null,
+			identifier: null,
+			options: this.defaultOptions('register', driverId),
+			secret: this.defaultSecret('register', driverId)
+		});
 	}
 
 	onClickUpdate(driverId, driverName) {
-		this.setState({ operation: 'update', driverId: driverId, driverName: driverName, jobId: null, options: this.defaultOptions('update', driverId), secret: this.defaultSecret('update', driverId) });
+		this.setState({
+			operation: 'update',
+			driverId: driverId,
+			driverName: driverName,
+			jobId: null,
+			identifier: null,
+			options: this.defaultOptions('update', driverId),
+			secret: this.defaultSecret('update', driverId)
+		});
 	}
 
 	onClickDeactivate(driverId, driverName) {
-		this.setState({ operation: 'deactivate', driverId: driverId, driverName: driverName, jobId: null, options: this.defaultOptions('deactivate', driverId), secret: this.defaultSecret('deactivate', driverId) });
+		this.setState({
+			operation: 'deactivate',
+			driverId: driverId,
+			driverName: driverName,
+			jobId: null,
+			identifier: null,
+			options: this.defaultOptions('deactivate', driverId),
+			secret: this.defaultSecret('deactivate', driverId)
+		});
 	}
 
 	onClickExecute() {
@@ -79,126 +132,189 @@ export class RegistrarInput extends Component {
 		this.execute();
 	}
 
+	onClickJobCheck(e) {
+		this.props.onLoading();
+		this.execute();
+		e.preventDefault();
+	}
+
 	onClickClear() {
-		this.setState({ operation: null, driverId: null, driverName: null, jobId: null, options: null, secret: null, addServices: [], addPublicKeys: [], addAuthentications: [] });
+		this.setState({
+			operation: null,
+			driverId: null,
+			driverName: null,
+			jobId: null,
+			identifier: null,
+			options: null,
+			secret: null,
+			addServices: [],
+			addPublicKeys: [],
+			addAuthentications: []
+		});
 		this.props.onClear();
 	}
 
 	onChangeOptions(e) {
-		this.setState({ options: e.target.value });
+		this.setState({options: e.target.value});
 	}
 
 	onChangeSecret(e) {
-		this.setState({ secret: e.target.value });
+		this.setState({secret: e.target.value});
+	}
+
+	onChangeJobId(e) {
+		this.setState({jobId: e.target.value});
+	}
+
+	onChangeIdentifier(e) {
+		this.setState({identifier: e.target.value});
 	}
 
 	onAddService(service) {
-		this.setState({ addServices: [ ...this.state.addServices, service ] });
+		this.setState({addServices: [...this.state.addServices, service]});
 	}
 
 	onAddPublicKey(publicKey) {
-		this.setState({ addPublicKeys: [ ...this.state.addPublicKeys, publicKey ] });
+		this.setState({addPublicKeys: [...this.state.addPublicKeys, publicKey]});
 	}
 
 	onAddAuthentication(authentication) {
-		this.setState({ addAuthentications: [ ...this.state.addAuthentications, authentication ] });
+		this.setState({addAuthentications: [...this.state.addAuthentications, authentication]});
 	}
 
 	onRemoveService(i) {
 		var addServices = this.state.addServices;
 		addServices.splice(i, 1);
-		this.setState({ addServices: addServices });
+		this.setState({addServices: addServices});
 	}
 
 	render() {
-		var jobIdItem;
-		if (this.state.jobId) {
-			jobIdItem = (
-				<Segment>
-					<span className="jobid-label">JOB ID: </span>
-					<span className="jobid">{this.state.jobId}</span>
-				</Segment>
+		var jobIdInput;
+		if (this.state.operation) {
+			jobIdInput = (
+				<Grid.Column width='10'>
+					<Form onSubmit={this.onClickJobCheck.bind(this)}>
+						<Form.Group inline>
+							<Form.Input label='JOB ID:' placeholder='...'
+										width='6'
+										value={this.state.jobId}
+										onChange={this.onChangeJobId.bind(this)}/>
+							<Form.Button primary>Check State</Form.Button>
+						</Form.Group>
+					</Form>
+				</Grid.Column>
 			);
 		}
 
 		var executeClearButtons;
 		if (this.state.operation && this.state.driverId) {
 			executeClearButtons = (
-				<Item>
-					{jobIdItem}
-					<Button primary onClick={this.onClickExecute.bind(this)}>{this.state.operation} → {this.state.driverName}</Button>
-					<Button secondary onClick={this.onClickClear.bind(this)}>Clear</Button>
-				</Item>
-			);
+				<Grid columns='16' divided>
+					<Grid.Row>
+						<Grid.Column width='4'>
+							<Item>
+								<Button primary
+										onClick={this.onClickExecute.bind(this)}>{this.state.operation} → {this.state.driverName}</Button>
+								<Button secondary onClick={this.onClickClear.bind(this)}>Clear</Button>
+							</Item>
+						</Grid.Column>
+						{jobIdInput}
+					</Grid.Row>
+				</Grid>
+			)
+			;
 		}
+
 
 		var registerButtons;
 		var updateButtons;
 		var deactivateButtons;
-		if (! this.state.operation) {
+		if (!this.state.operation) {
 			const registerButtonsList = this.props.drivers.map((driver, i) =>
-				<Button className="operationButton" primary key={i} onClick={this.onClickRegister.bind(this, driver.id, driver.name)}>{driver.name}</Button>
+				<Button className="operationButton" primary key={i}
+						onClick={this.onClickRegister.bind(this, driver.id, driver.name)}>{driver.name}</Button>
 			);
 			registerButtons = (
-				<Item className="buttons"><Label><label htmlFor={'registerButtonsList'}>Register:</label></Label><span id={'registerButtonsList'}>{registerButtonsList}</span></Item>
+				<Item className="buttons"><Label><label htmlFor={'registerButtonsList'}>Register:</label></Label><span
+					id={'registerButtonsList'}>{registerButtonsList}</span></Item>
 			);
 			const updateButtonsList = this.props.drivers.map((driver, i) =>
-				<Button className="operationButton" primary disabled key={i} onClick={this.onClickUpdate.bind(this, driver.id, driver.name)}>{driver.name}</Button>
+				<Button className="operationButton" primary key={i}
+						onClick={this.onClickUpdate.bind(this, driver.id, driver.name)}>{driver.name}</Button>
 			);
 			updateButtons = (
-				<Item className="buttons"><Label><label htmlFor={'updateButtonsList'}>Update:</label></Label><span id={'updateButtonsList'}>{updateButtonsList}</span></Item>
+				<Item className="buttons"><Label><label htmlFor={'updateButtonsList'}>Update:</label></Label><span
+					id={'updateButtonsList'}>{updateButtonsList}</span></Item>
 			);
 			const deactivateButtonsList = this.props.drivers.map((driver, i) =>
-				<Button className="operationButton" primary disabled key={i} onClick={this.onClickDeactivate.bind(this, driver.id, driver.name)}>{driver.name}</Button>
+				<Button className="operationButton" primary key={i}
+						onClick={this.onClickDeactivate.bind(this, driver.id, driver.name)}>{driver.name}</Button>
 			);
 			deactivateButtons = (
-				<Item className="buttons"><Label><label htmlFor={'deactivateButtonsList'}>Deactivate:</label></Label><span id={'deactivateButtonsList'}>{deactivateButtonsList}</span></Item>
+				<Item className="buttons"><Label><label
+					htmlFor={'deactivateButtonsList'}>Deactivate:</label></Label><span
+					id={'deactivateButtonsList'}>{deactivateButtonsList}</span></Item>
+			);
+		}
+
+		var identifierInput;
+		if ("update" === this.state.operation || "deactivate" === this.state.operation) {
+			identifierInput = (
+				<Input focus
+					   label="Identifier* "
+					   value={this.state.identifier}
+					   placeholder="DID..."
+					   onChange={this.onChangeIdentifier.bind(this)}/>
 			);
 		}
 
 		var optionsInput;
 		if (this.state.options !== null) {
 			optionsInput = (
-				<td>
+				<Table.Cell>
 					<Item>
 						<Item className="options-label" for={'optionsInput'}>OPTIONS:</Item>
-						<TextArea id={'optionsInput'} className="options" value={this.state.options} cols='60' rows='5' onChange={this.onChangeOptions.bind(this)} />
+						<TextArea id={'optionsInput'} className="options" value={this.state.options} cols='60' rows='5'
+								  onChange={this.onChangeOptions.bind(this)}/>
 					</Item>
-				</td>
+				</Table.Cell>
 			);
 		}
 
 		var secretInput;
 		if (this.state.secret != null) {
 			secretInput = (
-				<td>
+				<Table.Cell>
 					<Item>
 						<Item className="secret-label" for={'secretInput'}>SECRET:</Item>
-						<TextArea id={'secretInput'} className="secret" value={this.state.secret} cols='60' rows='5' onChange={this.onChangeSecret.bind(this)} />
+						<TextArea id={'secretInput'} className="secret" value={this.state.secret} cols='60' rows='5'
+								  onChange={this.onChangeSecret.bind(this)}/>
 					</Item>
-				</td>
+				</Table.Cell>
 			);
 		}
 
 		var optionsSecretInput;
 		if (optionsInput || secretInput) {
 			optionsSecretInput = (
-				<table>
-					<tr>
-						{optionsInput}
-						{secretInput}
-					</tr>
-				</table>
+				<Table>
+					<Table.Row>
+						<Item.Group>
+							{optionsInput}
+							{secretInput}
+						</Item.Group>
+					</Table.Row>
+				</Table>
 			);
 		}
 
 		var addServicesContainer;
 		if ("register" === this.state.operation || "update" === this.state.operation) {
 			const addService = (
-				<AddService onAddService={this.onAddService.bind(this)} />
+				<AddService onAddService={this.onAddService.bind(this)}/>
 			);
 			const addPublicKey = (
-				<AddPublicKey onAddPublicKey={this.onAddPublicKey.bind(this)} />
+				<AddPublicKey onAddPublicKey={this.onAddPublicKey.bind(this)}/>
 			);
 
 			const addServices = this.state.addServices.map((addService, i) =>
@@ -207,7 +323,7 @@ export class RegistrarInput extends Component {
 					i={i}
 					type={addService.type}
 					serviceEndpoint={addService.serviceEndpoint}
-					onRemoveService={this.onRemoveService.bind(this)} />
+					onRemoveService={this.onRemoveService.bind(this)}/>
 			);
 			var addServicesList;
 			if (Object.keys(addServices).length > 0) {
@@ -220,7 +336,7 @@ export class RegistrarInput extends Component {
 
 			addServicesContainer = (
 				<div>
-					<Divider />
+					<Divider/>
 					<label htmlFor={'services'} className="label">SERVICES:</label>
 					{addService}
 					{addServicesList}
@@ -234,6 +350,7 @@ export class RegistrarInput extends Component {
 				{registerButtons}
 				{updateButtons}
 				{deactivateButtons}
+				{identifierInput}
 				{optionsSecretInput}
 				{addServicesContainer}
 			</Segment>
@@ -243,49 +360,49 @@ export class RegistrarInput extends Component {
 	defaultOptions(operation, driverId) {
 		const options = {
 			'register': {
-				'driver-universalregistrar/driver-did-btcr': { 'chain': 'TESTNET' },
-				'driver-universalregistrar/driver-did-sov': { 'network': 'danube' },
-				'driver-universalregistrar/driver-did-v1': { 'ledger': 'test', 'keytype': 'ed25519' },
-				'driver-universalregistrar/driver-did-key': { 'keyType': 'Ed25519VerificationKey2018' }
+				'driver-universalregistrar/driver-did-btcr': {'chain': 'TESTNET'},
+				'driver-universalregistrar/driver-did-sov': {'network': 'danube'},
+				'driver-universalregistrar/driver-did-v1': {'ledger': 'test', 'keytype': 'ed25519'},
+				'driver-universalregistrar/driver-did-key': {'keyType': 'Ed25519VerificationKey2018'}
 			},
 			'update': {
-				'driver-universalregistrar/driver-did-btcr': { },
-				'driver-universalregistrar/driver-did-sov': { },
-				'driver-universalregistrar/driver-did-v1': { 'ledger': 'test' },
-				'driver-universalregistrar/driver-did-key': { }
+				'driver-universalregistrar/driver-did-btcr': {'chain': 'TESTNET'},
+				'driver-universalregistrar/driver-did-sov': {},
+				'driver-universalregistrar/driver-did-v1': {'ledger': 'test'},
+				'driver-universalregistrar/driver-did-key': {}
 			},
 			'deactivate': {
-				'driver-universalregistrar/driver-did-btcr': { 'chain': 'TESTNET' },
-				'driver-universalregistrar/driver-did-sov': { 'network': 'danube' },
-				'driver-universalregistrar/driver-did-v1': { 'ledger': 'test' },
-				'driver-universalregistrar/driver-did-key': { }
+				'driver-universalregistrar/driver-did-btcr': {'chain': 'TESTNET'},
+				'driver-universalregistrar/driver-did-sov': {'network': 'danube'},
+				'driver-universalregistrar/driver-did-v1': {'ledger': 'test'},
+				'driver-universalregistrar/driver-did-key': {}
 			}
 		};
-		return options[operation][driverId] ? JSON.stringify(options[operation][driverId], null, 2): null;
+		return options[operation][driverId] ? JSON.stringify(options[operation][driverId], null, 2) : null;
 	}
 
 	defaultSecret(operation, driverId) {
 		const secret = {
 			'register': {
-				'driver-universalregistrar/driver-did-btcr': { 'privateKeyWiF': null },
-				'driver-universalregistrar/driver-did-sov': { 'seed': null },
-				'driver-universalregistrar/driver-did-v1': { },
-				'driver-universalregistrar/driver-did-key': { }
+				'driver-universalregistrar/driver-did-btcr': {'privateKeyWiF': null},
+				'driver-universalregistrar/driver-did-sov': {'seed': null},
+				'driver-universalregistrar/driver-did-v1': {},
+				'driver-universalregistrar/driver-did-key': {}
 			},
 			'update': {
-				'driver-universalregistrar/driver-did-btcr': { 'privateKeyWiF': '...' },
-				'driver-universalregistrar/driver-did-sov': { 'seed': '...' },
-				'driver-universalregistrar/driver-did-v1': { },
-				'driver-universalregistrar/driver-did-key': { }
+				'driver-universalregistrar/driver-did-btcr': {'privateKeyWiF': '...'},
+				'driver-universalregistrar/driver-did-sov': {'seed': '...'},
+				'driver-universalregistrar/driver-did-v1': {},
+				'driver-universalregistrar/driver-did-key': {}
 			},
 			'deactivate': {
-				'driver-universalregistrar/driver-did-btcr': { 'privateKeyWiF': '...' },
-				'driver-universalregistrar/driver-did-sov': { 'seed': '...' },
-				'driver-universalregistrar/driver-did-v1': { },
-				'driver-universalregistrar/driver-did-key': { }
+				'driver-universalregistrar/driver-did-btcr': {'privateKeyWiF': '...'},
+				'driver-universalregistrar/driver-did-sov': {'seed': '...'},
+				'driver-universalregistrar/driver-did-v1': {},
+				'driver-universalregistrar/driver-did-key': {}
 			}
 		};
-		return secret[operation][driverId] ? JSON.stringify(secret[operation][driverId], null, 2): null;
+		return secret[operation][driverId] ? JSON.stringify(secret[operation][driverId], null, 2) : null;
 	}
 }
 
