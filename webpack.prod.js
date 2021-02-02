6c162dd493aa4dbdd738318048dd35a6f6022756
Markus Sabadello <markus@danubetech.com>
2021-02-02 12:17:54 +0100

    chore: Update dependencies.
    Signed-off-by: Markus Sabadello <markus@danubetech.com>

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,13 +1,18 @@
 const webpack = require('webpack');
-const {merge} = require('webpack-merge');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
 
+const TerserPlugin = require('terser-webpack-plugin');
+
 module.exports = merge(common, {
+  mode: 'production',
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
   plugins: [
-    new UglifyJSPlugin(),
     new webpack.DefinePlugin({
-      'env.backendUrl': JSON.stringify('/')
+      'env.backendUrl': JSON.stringify('/'),
     })
   ]
 });
